cf57fd0b9f4928b5910f543b7680665889d2a357
Charlie Dawson <charlie@charlesddawson.com>
2019-02-09 10:25:11 -0500

    Commit to upload before creating a new branch for new LED Strip

--- rpm_receive.ino
+++ rpm_receive.ino
@@ -22,7 +22,7 @@ unsigned char len = 0;
 unsigned char rxBuf[8];             // CAN message (8 bytes)
 char msgString[128];                // Array to store serial string
 
-#define CAN0_INT 2                  // Set INT to pin 2
+#define CAN0_INT 8                  // Set INT to pin 2
 MCP_CAN CAN0(10);                   // Set CS to pin 10
 
 int engineRpm = 0;
